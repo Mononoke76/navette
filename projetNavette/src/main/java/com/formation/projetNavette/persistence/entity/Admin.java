56a9836c6bfa19ed11e7e32f5e53d3cafb65f7b1
Dell <Dell@10.31.70.217>
2019-01-28 16:03:36 +0100

    ajout Entity Admin

---
+++ projetNavette/src/main/java/com/formation/projetNavette/persistence/entity/Admin.java
@@ -0,0 +1,50 @@
+package com.formation.projetNavette.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Admin")
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="login")
+	private String login;
+	
+	@Column(name="password")
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
